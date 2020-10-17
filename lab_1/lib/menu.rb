16f238b04ace924fa72fdc757b1176fc1d5e9835
C0nfirmeD <lioxa.zhik@gmail.com>
2020-10-17 22:36:42 +0300

    add ability to list all the trains

--- lab_1/lib/menu.rb
+++ lab_1/lib/menu.rb
@@ -1,11 +1,26 @@
 # frozen_string_literal: true
 
 require 'tty-prompt'
+require_relative 'train_list'
+require_relative 'train'
+require_relative 'stop'
 
 # The main class
 class Menu
   def initialize
     @prompt = TTY::Prompt.new
+    @train_list = TrainList.new([
+                                  Train.new(155, [
+                                              Stop.new(1, 'Some', '12:55', 10, 10),
+                                              Stop.new(2, 'Some1', '13:55', 10, 10),
+                                              Stop.new(3, 'Some2', '14:55', 10, 10)
+                                            ]),
+                                  Train.new(110, [
+                                              Stop.new(4, 'First', '12:05', 10, 10),
+                                              Stop.new(5, 'Sec', '13:05', 10, 10),
+                                              Stop.new(6, 'Third', '14:05', 10, 10)
+                                            ])
+                                ])
   end
 
   MAIN_MENU_CHOICES = [
@@ -18,6 +33,17 @@ class Menu
     loop do
       action = @prompt.select('Выберите действие', MAIN_MENU_CHOICES)
       break if action == :exit
+
+      show_rasp if action == :rasp
     end
+  end
+
+  def show_rasp
+    train_id = @prompt.select('Выберите поезд') do |menu|
+      @train_list.each_train do |train|
+        menu.choice(train, train.id)
+      end
+    end
+    pp train_id
   end
 end
